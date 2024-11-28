873c096802f6d6f7c99c24aab67cefa8d306d4b1
MANANJAY ROY <mananjay21roy@gmail.com>
2024-11-28 17:45:04 +0530

    Folder structure and route section added

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+
+function Home() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Welcome to KFC!</h1>
+            <p>This is the home page.</p>
+            {/* Add your home page content here */}
+        </div>
+    );
+}
+
+export default Home;
